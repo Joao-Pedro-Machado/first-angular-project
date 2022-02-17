74fc86118ee84756f7a34e48a8d5c20c4862136f
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-02-17 08:28:16 -0300

    creating photos module and using ngFor to load data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
-import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
