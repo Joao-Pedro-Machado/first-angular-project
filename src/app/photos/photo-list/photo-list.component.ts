23223aeddb25d9a1fefefa64ca07212ac52b21f5
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-02-18 09:02:41 -0300

    Creating photos filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 
-import { PhotoService } from '../photo/photo.service'
 
 @Component({
   selector: 'app-photo-list',
@@ -12,19 +11,21 @@ import { PhotoService } from '../photo/photo.service'
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  onKeyUp(target: any) {
+
+    if(target instanceof EventTarget){
+      const element = target as HTMLInputElement;
+      this.filter = element.value
+    }
+
+  }
 
   ngOnInit(): void {
-
-    const userName = this.activatedRoute.snapshot.params['userName']
-    this.photoService.listFromUser(userName).subscribe(photos => {
-      this.photos = photos
-    })
-
+    this.photos = this.activatedRoute.snapshot.data['photos']
   }
 
 }
