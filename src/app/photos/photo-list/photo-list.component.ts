b589322dcc99ecdd575ba47a07df85f682f3c5cf
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-02-18 09:33:13 -0300

    Implementing debounceTime to the filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>()
 
   constructor(private activatedRoute: ActivatedRoute) { }
+  
 
-  onKeyUp(target: any) {
+  onKeyUp(target: any): any {
 
     if(target instanceof EventTarget){
       const element = target as HTMLInputElement;
-      this.filter = element.value
-    }
 
+      return element.value
+    }
+    
   }
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos']
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter)
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe()
   }
 
 }
